98d1e3a51ec40e35b8603aed4d2e1848fe27bec6
JishnuCode2 <88920598+JishnuCode2@users.noreply.github.com>
2023-04-15 22:34:57 +0530

    Update bowling.js

--- components/bowling.js
+++ components/bowling.js
@@ -1,18 +1,18 @@
 AFRAME.registerComponent("shoot",{
     init: function(){
-        this.s()
+        this.shoot()
     },
-    s: function(){
+    shoot: function(){
         window.addEventListener("keydown", (e)=>{
             if( e.key === "z"){
-                var bullet = document.createElement("a-entity")
+                var ball = document.createElement("a-entity")
 
-                bullet.setAttribute("gltf-model", "./models/models/bowling_ball/scene.gltf")
+                ball.setAttribute("gltf-model", "./models/models/bowling_ball/scene.gltf")
 
                 var cam = document.querySelector("#camera");
 
                 var pos = cam.getAttribute("position")
-                bullet.setAttribute("position", {x:pos.x, y: 0, z: (pos.z - 0.7)});
+                ball.setAttribute("position", {x:pos.x, y: 0, z: (pos.z - 0.7)});
 
                 var camera = document.querySelector("#camera").object3D
 
@@ -21,9 +21,9 @@ AFRAME.registerComponent("shoot",{
                 var direction = new THREE.Vector3()
                 camera.getWorldDirection(direction);
 
-                bullet.setAttribute("velocity", direction.multiplyScalar(-1))
+                ball.setAttribute("velocity", direction.multiplyScalar(-1))
 
-                scene.appendChild(bullet)
+                scene.appendChild(ball)
 
                 
             }
